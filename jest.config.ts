15d15e233a770e8d5c8d1ef6acad9c618ecf8ae9
Paulo Sergio <pauloserg861@gmail.com>
2025-07-18 17:26:26 -0300

    chore: config jest to run unit tests

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/**/*.module.ts',
+    '!<rootDir>/src/domain/**',
+    '!<rootDir>/src/main/**',
+  ],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  clearMocks: true,
+  //preset: '@shelf/jest-mongodb',
+  transform: {
+    '^.+\\.(t|j)s$': '@swc/jest',
+  },
+  moduleNameMapper: {
+    '@/test/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+};
+
+export default config;
