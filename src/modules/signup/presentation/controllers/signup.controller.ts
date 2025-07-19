225a3cfd041a79258c6b7d1286594d6db66a8be5
Paulo Sergio <pauloserg861@gmail.com>
2025-07-18 21:45:04 -0300

    feat: ensure SignupController returns 400 if no name is provided

--- src/modules/signup/presentation/controllers/signup.controller.ts
+++ src/modules/signup/presentation/controllers/signup.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
 
 @Controller('signup')
-export class SignupController {}
+export class SignupController {
+  async handle(@Body() request: any): Promise<any> {
+    if (!request.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param name'),
+      };
+    }
+  }
+}
