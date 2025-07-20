0aa49e85a42276710e98e30f8354531ec0f65f98
Paulo Sergio <pauloserg861@gmail.com>
2025-07-19 22:00:39 -0300

    feat: ensure Account is created with free plan as default

--- src/modules/account/domain/entities/Account.ts
+++ src/modules/account/domain/entities/Account.ts
@@ -1,1 +1,15 @@
-export class Account {}
+type AccountProps = {
+  plan?: Account.Plan;
+};
+export class Account {
+  constructor(public readonly props: AccountProps) {
+    this.props.plan = props.plan || Account.Plan.FREE;
+  }
+}
+
+export namespace Account {
+  export enum Plan {
+    FREE = 'free',
+    PREMIUM = 'premium',
+  }
+}
