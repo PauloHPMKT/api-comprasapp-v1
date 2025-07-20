1ad56b025dadb799844c259f31a0221430bc8f0b
Paulo Sergio <pauloserg861@gmail.com>
2025-07-20 13:38:53 -0300

    test: ensure deepFreeze not freeze a scalar boolean value

--- src/shared/utils/object.spec.ts
+++ src/shared/utils/object.spec.ts
@@ -1,8 +1,13 @@
 import { deepFreeze } from './object';
 
 describe('Object deepFreeze Utility', () => {
-  it('should not freeze a scalar value', () => {
+  it('should not freeze a scalar string value', () => {
     const str = deepFreeze('test');
     expect(typeof str).toBe('string');
+  });
+
+  it('should not freeze a scalar boolean value', () => {
+    const bool = deepFreeze(true);
+    expect(typeof bool).toBe('boolean');
   });
 });
