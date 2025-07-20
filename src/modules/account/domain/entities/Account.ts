b08c6d3c21f75b0c92f8729abb36978b54a45f00
Paulo Sergio <pauloserg861@gmail.com>
2025-07-19 22:18:32 -0300

    feat: ensure Account is created with userId null

--- src/modules/account/domain/entities/Account.ts
+++ src/modules/account/domain/entities/Account.ts
@@ -1,11 +1,13 @@
 type AccountProps = {
   plan?: Account.Plan;
   isActive?: boolean;
+  userId?: string | null;
 };
 export class Account {
   constructor(public readonly props: AccountProps) {
     this.props.plan = props.plan || Account.Plan.FREE;
     this.props.isActive = props.isActive ?? true;
+    this.props.userId = props.userId ?? null;
   }
 }
 
