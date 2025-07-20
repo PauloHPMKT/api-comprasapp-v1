e511317e8afc6efd9ed71296063e2b2d694a37dc
Paulo Sergio <pauloserg861@gmail.com>
2025-07-19 22:20:53 -0300

    feat: ensure Account is created with a valid date

--- src/modules/account/domain/entities/Account.ts
+++ src/modules/account/domain/entities/Account.ts
@@ -2,12 +2,14 @@ type AccountProps = {
   plan?: Account.Plan;
   isActive?: boolean;
   userId?: string | null;
+  createdAt?: Date;
 };
 export class Account {
   constructor(public readonly props: AccountProps) {
     this.props.plan = props.plan || Account.Plan.FREE;
     this.props.isActive = props.isActive ?? true;
     this.props.userId = props.userId ?? null;
+    this.props.createdAt = props.createdAt ?? new Date();
   }
 }
 
