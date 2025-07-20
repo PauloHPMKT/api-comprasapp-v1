9d9a13b93fe6238d109a78647e236949326006b2
Paulo Sergio <pauloserg861@gmail.com>
2025-07-19 22:52:14 -0300

    feat: add value object to manager password characterist

--- src/shared/@seedworks/domain/value-objects/Password.ts
+++ src/shared/@seedworks/domain/value-objects/Password.ts
@@ -1,1 +1,20 @@
-export class Password {}
+export class Password {
+  private readonly minLength = 6;
+
+  constructor(public readonly _value?: string) {
+    this._value = _value;
+    this.validate(this._value);
+  }
+
+  validate(password: string): void {
+    if (password.length < this.minLength) {
+      throw new Error(
+        `Password must be at least ${this.minLength} characters long`,
+      );
+    }
+  }
+
+  get value(): string {
+    return this._value;
+  }
+}
