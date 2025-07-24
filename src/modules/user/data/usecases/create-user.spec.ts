1a4264cbe433de178cd8ae73abc8a16be403a10a
Paulo Sergio <pauloserg861@gmail.com>
2025-07-24 11:56:19 -0300

    test: ensure CreateUserUseCase throws if user already exists

--- src/modules/user/data/usecases/create-user.spec.ts
+++ src/modules/user/data/usecases/create-user.spec.ts
@@ -1,20 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateUserUseCase } from './create-user';
 
-// const makeMocks = () => ({});
+const makeMocks = () => ({
+  isExistsUserRepositoryStub: {
+    exists: jest.fn().mockResolvedValue(false),
+  },
+});
 
 const makeSut = async (): Promise<SutTypes> => {
+  const { isExistsUserRepositoryStub } = makeMocks();
   const moduleRef: TestingModule = await Test.createTestingModule({
-    providers: [CreateUserUseCase],
+    providers: [
+      CreateUserUseCase,
+      {
+        provide: 'IsExistsUserRepositoryPort',
+        useValue: isExistsUserRepositoryStub,
+      },
+    ],
   }).compile();
   const sut = moduleRef.get<CreateUserUseCase>(CreateUserUseCase);
   return {
     sut,
+    isExistsUserRepositoryStub,
   };
 };
 
 type SutTypes = {
   sut: CreateUserUseCase;
+  isExistsUserRepositoryStub: { exists: jest.Mock };
 };
 
 describe('CreateUserUseCase', () => {
@@ -23,5 +36,17 @@ describe('CreateUserUseCase', () => {
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(CreateUserUseCase);
     expect(sut).toBeTruthy();
+  });
+
+  it('should throw an error if user already exists', async () => {
+    const { sut, isExistsUserRepositoryStub } = await makeSut();
+    const params = {
+      name: 'Test User',
+      email: 'any_email@mail.com',
+    };
+    jest
+      .spyOn(isExistsUserRepositoryStub, 'exists')
+      .mockResolvedValueOnce(true);
+    await expect(sut.execute(params)).rejects.toThrow('User already exists');
   });
 });
