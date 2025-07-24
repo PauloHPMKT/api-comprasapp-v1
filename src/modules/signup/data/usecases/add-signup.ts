8489ce77e3e71a5840f3250ea0dbe349b52d0a08
Paulo Sergio <pauloserg861@gmail.com>
2025-07-23 22:26:04 -0300

    refactor: ensure AddSignupUseCase thows if passwords not match

--- src/modules/signup/data/usecases/add-signup.ts
+++ src/modules/signup/data/usecases/add-signup.ts
@@ -15,14 +15,15 @@ export class AddSignupUseCase implements AddAccount {
   ) {}
 
   async execute(params: SignupModel.Params): Promise<string> {
+    if (params.password !== params.confirmationPassword) {
+      throw new Error('Password and confirmation password do not match');
+    }
+
+    // chama o caso de uso do usuario para criar o usuário
     const isUserExists = await this.isExistsUserRepositoryPort.exists(
       params.email,
     );
     if (isUserExists) throw new UserAlreadyExistsError();
-
-    if (params.password !== params.confirmationPassword) {
-      throw new Error('Password and confirmation password do not match');
-    }
 
     await this.encrypterPort.hash(params.password);
 
