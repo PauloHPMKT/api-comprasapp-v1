830aee7a8505a6669ad2eee4c90450228d9fa43b
Paulo Sergio <pauloserg861@gmail.com>
2025-07-24 23:17:18 -0300

    feat: ensure UserEntity is created with email

--- src/modules/user/domain/entities/User.ts
+++ src/modules/user/domain/entities/User.ts
@@ -1,9 +1,16 @@
-type UserProps = {
+export type UserProps = {
   name: string;
+  email: string;
 };
 
 export class User {
   constructor(public readonly props: UserProps) {}
 }
 
-console.log('User entity loaded', new User({ name: 'Test User' }));
+console.log(
+  'User entity loaded',
+  new User({
+    name: 'Test User',
+    email: 'any_email@mail.com',
+  }),
+);
