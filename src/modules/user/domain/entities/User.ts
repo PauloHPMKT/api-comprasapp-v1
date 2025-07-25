f20703eb102c2cbedce04194fed3127156181282
Paulo Sergio <pauloserg861@gmail.com>
2025-07-24 23:37:03 -0300

    test: ensure a user is created with a valid createdAt

--- src/modules/user/domain/entities/User.ts
+++ src/modules/user/domain/entities/User.ts
@@ -2,11 +2,13 @@ export type UserProps = {
   name: string;
   email: string;
   avatar?: string | null;
+  createdAt?: Date;
 };
 
 export class User {
   constructor(public readonly props: UserProps) {
     this.props.avatar = this.props.avatar ?? null;
+    this.props.createdAt = this.props.createdAt ?? new Date();
   }
 }
 
