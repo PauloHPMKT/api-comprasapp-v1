58356cf8c65b1c2d60170fc327281aa5ec4fbb47
Paulo Sergio <pauloserg861@gmail.com>
2025-07-26 11:41:34 -0300

    test: ensure CreateUserUseCase calls CreateUserRepository with correct parameters

--- src/modules/user/data/usecases/create-user.spec.ts
+++ src/modules/user/data/usecases/create-user.spec.ts
@@ -2,23 +2,34 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CreateUserUseCase } from './create-user';
 import { User } from '../../domain/entities/User';
 
-jest.mock('../../domain/entities/user', () => {
-  return {
-    User: jest.fn().mockImplementation((props) => ({
-      ...props,
-      mocked: true,
-    })),
-  };
-});
+jest.mock('../../domain/entities/user', () => ({
+  User: jest.fn().mockImplementation(() => {
+    return {
+      toJSON: jest.fn().mockReturnValue({
+        id: 'valid_id',
+        name: 'Test User',
+        email: 'valid_email@mail.com',
+        avatar: null,
+        createdAt: new Date('2025-01-01T00:00:00Z'),
+      }),
+    };
+  }),
+}));
 
 const makeMocks = () => ({
   isExistsUserRepositoryStub: {
     exists: jest.fn().mockResolvedValue(false),
   },
+  createUserRepositoryStub: {
+    create: jest.fn().mockResolvedValue({
+      id: 'valid_id',
+      email: 'valid_email@mail.com',
+    }),
+  },
 });
 
 const makeSut = async (): Promise<SutTypes> => {
-  const { isExistsUserRepositoryStub } = makeMocks();
+  const { isExistsUserRepositoryStub, createUserRepositoryStub } = makeMocks();
   const moduleRef: TestingModule = await Test.createTestingModule({
     providers: [
       CreateUserUseCase,
@@ -26,18 +37,24 @@ const makeSut = async (): Promise<SutTypes> => {
         provide: 'IsExistsUserRepositoryPort',
         useValue: isExistsUserRepositoryStub,
       },
+      {
+        provide: 'CreateUserRepositoryPort',
+        useValue: createUserRepositoryStub,
+      },
     ],
   }).compile();
   const sut = moduleRef.get<CreateUserUseCase>(CreateUserUseCase);
   return {
     sut,
     isExistsUserRepositoryStub,
+    createUserRepositoryStub,
   };
 };
 
 type SutTypes = {
   sut: CreateUserUseCase;
   isExistsUserRepositoryStub: { exists: jest.Mock };
+  createUserRepositoryStub: { create: jest.Mock };
 };
 
 describe('CreateUserUseCase', () => {
@@ -70,6 +87,23 @@ describe('CreateUserUseCase', () => {
     expect(User).toHaveBeenCalledWith({
       name: 'Test User',
       email: 'any_email@mail.com',
+    });
+  });
+
+  it('should call createUserRepository with correct parameters', async () => {
+    const { sut, createUserRepositoryStub } = await makeSut();
+    const params = {
+      name: 'Test User',
+      email: 'any_email@mail.com',
+    };
+    const createUserSpy = jest.spyOn(createUserRepositoryStub, 'create');
+    await sut.execute(params);
+    expect(createUserSpy).toHaveBeenCalledWith({
+      id: 'valid_id',
+      name: 'Test User',
+      email: 'valid_email@mail.com',
+      avatar: null,
+      createdAt: expect.any(Date),
     });
   });
 });
