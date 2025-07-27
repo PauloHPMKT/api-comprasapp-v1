110b47a1146c915b4df02916d7e79415015a4c4b
Paulo Sergio <pauloserg861@gmail.com>
2025-07-26 21:34:05 -0300

    test: ensure CreateAccountUseCase calls Account entity with corrects params

--- src/modules/account/data/usecases/create-account.spec.ts
+++ src/modules/account/data/usecases/create-account.spec.ts
@@ -1,5 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateAccountUseCase } from './create-account';
+import { Account } from '../../domain/entities/Account';
+
+jest.mock('../../domain/entities/account', () => ({
+  Account: jest.fn().mockImplementation(() => {
+    return {
+      toJSON: jest.fn().mockReturnValue({
+        id: 'valid_id',
+        plan: 'free',
+        isActive: true,
+        userId: '507f1f77bcf86cd799439012',
+        password: 'hashed_password',
+        createdAt: new Date('2025-01-01T00:00:00Z'),
+      }),
+    };
+  }),
+}));
 
 const makeMocks = () => ({
   isAccountActiveStub: {
@@ -45,5 +61,18 @@ describe('CreateAccountUseCase', () => {
       password: 'any_password',
     };
     await expect(sut.execute(params)).rejects.toThrow('Account is not active');
+  });
+
+  it('should call Account Entity with correct parameters', async () => {
+    const { sut } = await makeSut();
+    const params = {
+      userId: '507f1f77bcf86cd799439012',
+      password: 'secure_password',
+    };
+    await sut.execute(params);
+    expect(Account).toHaveBeenCalledWith({
+      userId: '507f1f77bcf86cd799439012',
+      password: 'secure_password',
+    });
   });
 });
