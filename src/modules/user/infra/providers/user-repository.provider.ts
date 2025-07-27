761e6525cd4c9d48bf19c4b3a05a6f56d5892dde
Paulo Sergio <pauloserg861@gmail.com>
2025-07-27 15:44:49 -0300

    refactor: adjust import repository

--- src/modules/user/infra/providers/user-repository.provider.ts
+++ src/modules/user/infra/providers/user-repository.provider.ts
@@ -1,33 +1,13 @@
 import { Provider } from '@nestjs/common';
-import { IsExistsUserRepositoryPort } from '../../data/ports/is-exists-user.repository';
-import { CreateUserRepositoryPort } from '../../data/ports/create-user.repository';
-import { CreateUserRepositoryModel } from '../../data/models/user-repository.model';
-
-export class UserRepository
-  implements IsExistsUserRepositoryPort, CreateUserRepositoryPort
-{
-  async exists(email: string): Promise<boolean> {
-    // Simulate a database check
-    console.log(`Checking if user exists with email: ${email}`);
-    return false;
-  }
-
-  async create(
-    userData: CreateUserRepositoryModel.Params,
-  ): Promise<CreateUserRepositoryModel.Result> {
-    // Simulate user creation
-    console.log(`Creating user with data: ${JSON.stringify(userData)}`);
-    return { id: userData.id, email: userData.email };
-  }
-}
+import { MongoUserRepository } from '../repository/mongo-user.repository';
 
 export const makeUserRepositoryProviders = (): Provider[] => [
   {
     provide: 'IsExistsUserRepositoryPort',
-    useClass: UserRepository,
+    useClass: MongoUserRepository,
   },
   {
     provide: 'CreateUserRepositoryPort',
-    useClass: UserRepository,
+    useClass: MongoUserRepository,
   },
 ];
