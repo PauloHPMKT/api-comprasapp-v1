98437c7b675bcc141b210bfe0e16b8af07de6a94
Paulo Sergio <pauloserg861@gmail.com>
2025-07-27 23:27:00 -0300

    test: ensure exists method in MongoUserRepository return true if a user already exists

--- src/modules/user/infra/repository/mongo-user.repository.spec.ts
+++ src/modules/user/infra/repository/mongo-user.repository.spec.ts
@@ -1,23 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MongoUserRepository } from './mongo-user.repository';
+import { MongoHelper } from '@/modules/database/mongodb/helpers/mongo-helper';
 
 const makeSut = async (): Promise<SutTypes> => {
+  const findOneMock = jest.fn();
+
+  jest.spyOn(MongoHelper, 'getCollection').mockReturnValue({
+    findOne: findOneMock,
+  } as any);
+
   const moduleRef: TestingModule = await Test.createTestingModule({
     providers: [MongoUserRepository],
   }).compile();
+
   const sut = moduleRef.get<MongoUserRepository>(MongoUserRepository);
-  return { sut };
+
+  return { sut, findOneMock };
 };
 
 type SutTypes = {
   sut: MongoUserRepository;
+  findOneMock: jest.Mock;
 };
 
 describe('MongoUserRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('users');
+    await accountCollection.deleteMany({});
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it('should be defined', async () => {
     const { sut } = await makeSut();
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(MongoUserRepository);
     expect(sut).toBeTruthy();
+  });
+
+  it('deve retornar true quando o usuário existir', async () => {
+    const { sut, findOneMock } = await makeSut();
+    findOneMock.mockResolvedValueOnce({ _id: 'any_id' });
+    const result = await sut.exists('any_email@mail.com');
+
+    expect(result).toBe(true);
+    expect(MongoHelper.getCollection).toHaveBeenCalledWith('users');
+    expect(findOneMock).toHaveBeenCalledWith(
+      { email: 'any_email@mail.com' },
+      { projection: { _id: 1 } },
+    );
   });
 });
