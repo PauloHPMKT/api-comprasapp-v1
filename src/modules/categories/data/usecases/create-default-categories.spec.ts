cf5af824c26d04f2419f1dbfec029ac1e752f14a
Paulo Sergio <pauloserg861@gmail.com>
2025-08-03 14:49:29 -0300

    test: ensure CreateDefaultCategories calls CreateDefaultCategoriesRepository with correct values

--- src/modules/categories/data/usecases/create-default-categories.spec.ts
+++ src/modules/categories/data/usecases/create-default-categories.spec.ts
@@ -1,20 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateDefaultCategoriesUseCase } from './create-default-categories';
 
+const makeMocks = () => ({
+  createDefaultCategoriesRepositoryStub: {
+    create: jest.fn().mockResolvedValue(undefined),
+  },
+});
+
 const makeSut = async (): Promise<SutTypes> => {
+  const { createDefaultCategoriesRepositoryStub } = makeMocks();
+
   const moduleRef: TestingModule = await Test.createTestingModule({
-    providers: [CreateDefaultCategoriesUseCase],
+    providers: [
+      CreateDefaultCategoriesUseCase,
+      {
+        provide: 'CreateDefaultCategoriesRepositoryPort',
+        useValue: createDefaultCategoriesRepositoryStub,
+      },
+    ],
   }).compile();
   const sut = moduleRef.get<CreateDefaultCategoriesUseCase>(
     CreateDefaultCategoriesUseCase,
   );
   return {
     sut,
+    createDefaultCategoriesRepositoryStub,
   };
 };
 
 type SutTypes = {
   sut: CreateDefaultCategoriesUseCase;
+  createDefaultCategoriesRepositoryStub: {
+    create: jest.Mock;
+  };
 };
 
 describe('CreateDefaultCategoriesUseCase', () => {
@@ -33,4 +51,30 @@ describe('CreateDefaultCategoriesUseCase', () => {
       await expect(promise).rejects.toThrow('Account ID is required');
     },
   );
+
+  it('should call CreateDefaultCategoriesRepository with correct ', async () => {
+    const { sut, createDefaultCategoriesRepositoryStub } = await makeSut();
+    const accountId = 'valid_account_id';
+    const categories = [
+      { name: 'Hortifrutti', emoji: '🥦' },
+      { name: 'Mercearia', emoji: '🛒' },
+      { name: 'Limpeza', emoji: '🧽' },
+      { name: 'Higiene', emoji: '🧼' },
+      { name: 'Bebidas', emoji: '🍹' },
+      { name: 'Padaria', emoji: '🍞' },
+      { name: 'Carnes', emoji: '🥩' },
+      { name: 'Outros', emoji: '❓' },
+    ];
+    const createDefaultCategoriesSpy = jest.spyOn(
+      createDefaultCategoriesRepositoryStub,
+      'create',
+    );
+    const expectedCategories = categories.map((category) => ({
+      ...category,
+      accountId,
+      createdAt: expect.any(Date),
+    }));
+    sut.execute(accountId);
+    expect(createDefaultCategoriesSpy).toHaveBeenCalledWith(expectedCategories);
+  });
 });
