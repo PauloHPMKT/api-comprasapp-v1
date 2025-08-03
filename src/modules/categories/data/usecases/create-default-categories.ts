9c0c5e7788ef796e922a1bfc8d0193817af5db1e
Paulo Sergio <pauloserg861@gmail.com>
2025-08-03 12:50:40 -0300

    feat: ensure CreateDefaultCategories throw an error if account id is null

--- src/modules/categories/data/usecases/create-default-categories.ts
+++ src/modules/categories/data/usecases/create-default-categories.ts
@@ -1,1 +1,11 @@
-export class CreateDefaultCategoriesUseCase {}
+import { CreateDefaultCategoriesPort } from '../../domain/ports/create-default-categories.port';
+
+export class CreateDefaultCategoriesUseCase
+  implements CreateDefaultCategoriesPort
+{
+  async execute(accountId: string): Promise<void> {
+    if (!accountId) {
+      throw new Error('Account ID is required');
+    }
+  }
+}
