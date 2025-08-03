aec8a78acb9e0807d5d90f98a91c958bec25a16e
Paulo Sergio <pauloserg861@gmail.com>
2025-08-03 14:58:39 -0300

    test: ensure Category entity is defined

---
+++ src/modules/categories/domain/entities/category.spec.ts
@@ -0,0 +1,14 @@
+import { Category } from './Category';
+
+const makeSut = (): Category => {
+  return new Category();
+};
+
+describe('Category Entity', () => {
+  it('should be defined', () => {
+    const sut = makeSut();
+    expect(sut).toBeDefined();
+    expect(sut).toBeInstanceOf(Category);
+    expect(sut).toBeTruthy();
+  });
+});
