b24b89be950dd946bb55266c2856fcbe84cdcea6
Paulo Sergio <pauloserg861@gmail.com>
2025-08-24 12:44:51 -0300

    feat: ensure Category is created with a valid createdAt Date

--- src/modules/categories/domain/entities/Category.ts
+++ src/modules/categories/domain/entities/Category.ts
@@ -2,8 +2,11 @@ export type CategoryProps = {
   accountId: string;
   name: string;
   emoji: string;
+  createdAt?: Date;
 };
 
 export class Category {
-  constructor(public readonly props: CategoryProps) {}
+  constructor(public readonly props: CategoryProps) {
+    this.props.createdAt = props.createdAt ?? new Date();
+  }
 }
