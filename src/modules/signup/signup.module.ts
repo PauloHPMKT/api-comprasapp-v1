7fbf6f41067de760188961ef3da738cf4762e91f
Paulo Sergio <pauloserg861@gmail.com>
2025-08-24 10:59:20 -0300

    chore: change module filder position

--- src/modules/signup/signup.module.ts
+++ src/modules/signup/signup.module.ts
@@ -1,11 +1,12 @@
 import { Module, Provider } from '@nestjs/common';
-import { SignupController } from './controllers/signup.controller';
-import { makeUseCasesProviders } from '../domain/providers/usecases';
 import { UserModule } from '@/modules/user/presentation/user.module';
 import { AccountModule } from '@/modules/account/presentation/account.module';
 import { CategoriesModule } from '@/modules/categories/presentation/categories.module';
+import { makeUseCasesProviders } from './domain/providers/usecases';
+import { SignupController } from './presentation/controllers/signup.controller';
 
 const providers: Provider[] = [...makeUseCasesProviders()];
+
 @Module({
   imports: [UserModule, AccountModule, CategoriesModule],
   controllers: [SignupController],
