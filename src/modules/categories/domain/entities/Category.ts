76647bb9dd1a86d0bf58b34040374e8e6a50c14a
Paulo Sergio <pauloserg861@gmail.com>
2025-08-24 13:16:51 -0300

    feat: ensure Category is created with a valid unique entity id

--- src/modules/categories/domain/entities/Category.ts
+++ src/modules/categories/domain/entities/Category.ts
@@ -1,3 +1,6 @@
+import Entity from '@/shared/@seedworks/domain/entity/entity';
+import UniqueEntityId from '@/shared/@seedworks/domain/value-objects/unique-entity-id';
+
 export type CategoryProps = {
   accountId: string;
   name: string;
@@ -5,8 +8,12 @@ export type CategoryProps = {
   createdAt?: Date;
 };
 
-export class Category {
-  constructor(public readonly props: CategoryProps) {
+export class Category extends Entity<CategoryProps> {
+  constructor(
+    public override readonly props: CategoryProps,
+    id?: UniqueEntityId,
+  ) {
+    super(props, id);
     this.props.createdAt = props.createdAt ?? new Date();
   }
 }
