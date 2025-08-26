943e2ce62cee372d3cdd73b88750fc943ef1aeeb
Paulo Sergio <pauloserg861@gmail.com>
2025-08-26 08:54:43 -0300

    test: ensure MongoCategoryRepository insert many default categories on account creation

--- src/modules/categories/infra/repository/mongo-category.repository.spec.ts
+++ src/modules/categories/infra/repository/mongo-category.repository.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MongoHelper } from '@/modules/database/mongodb/helpers/mongo-helper';
 import { MongoCategoriesRepository } from './mongo-category.repository';
+import { ObjectId } from 'mongodb';
 
 const makeSut = async (): Promise<SutTypes> => {
   const findOneMock = jest.fn();
@@ -8,7 +9,7 @@ const makeSut = async (): Promise<SutTypes> => {
 
   jest.spyOn(MongoHelper, 'getCollection').mockReturnValue({
     findOne: findOneMock,
-    insertOne: insertManyMock,
+    insertMany: insertManyMock,
   } as any);
 
   const moduleRef: TestingModule = await Test.createTestingModule({
@@ -51,5 +52,44 @@ describe('MongoUserRepository', () => {
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(MongoCategoriesRepository);
     expect(sut).toBeTruthy();
+  });
+
+  it('should insert many default categories', async () => {
+    const { sut, insertManyMock } = await makeSut();
+    const categoryId = new ObjectId();
+    const categories = [
+      {
+        id: categoryId.toString(),
+        accountId: 'any_account_id',
+        name: 'Hortifrutti',
+        emoji: '🥦',
+        createdAt: new Date('2024-01-01T00:00:00Z'),
+      },
+      {
+        id: categoryId.toString(),
+        accountId: 'any_account_id',
+        name: 'Mercearia',
+        emoji: '🛒',
+        createdAt: new Date('2024-01-01T00:00:00Z'),
+      },
+    ];
+    await sut.addCategories(categories);
+    expect(insertManyMock).toHaveBeenCalledTimes(1);
+    expect(insertManyMock).toHaveBeenCalledWith([
+      {
+        _id: expect.any(ObjectId),
+        accountId: 'any_account_id',
+        name: 'Hortifrutti',
+        emoji: '🥦',
+        createdAt: new Date('2024-01-01T00:00:00Z'),
+      },
+      {
+        _id: expect.any(ObjectId),
+        accountId: 'any_account_id',
+        name: 'Mercearia',
+        emoji: '🛒',
+        createdAt: new Date('2024-01-01T00:00:00Z'),
+      },
+    ]);
   });
 });
