f8d5025b97795b6131a7b7bf9a99b431ef5e1361
Paulo Sergio <pauloserg861@gmail.com>
2025-08-30 21:21:54 -0300

    test: ensure CreateCategoryController return bad request if no name is provided

---
+++ src/modules/categories/presentation/controllers/create-category.controller.spec.ts
@@ -0,0 +1,37 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CreateCategoryController } from './create-category.controller';
+import { MissingParamError } from '@/shared/errors';
+
+const makeSut = async (): Promise<SutTypes> => {
+  const moduleRef: TestingModule = await Test.createTestingModule({
+    controllers: [CreateCategoryController],
+    providers: [],
+  }).compile();
+  const sut = moduleRef.get<CreateCategoryController>(CreateCategoryController);
+  return { sut };
+};
+
+type SutTypes = {
+  sut: CreateCategoryController;
+};
+
+describe('CreateCategoryController', () => {
+  it('should be defined', async () => {
+    const { sut } = await makeSut();
+    expect(sut).toBeDefined();
+    expect(sut).toBeInstanceOf(CreateCategoryController);
+    expect(sut).toBeTruthy();
+  });
+
+  it('should return bad request if no name is provided', async () => {
+    const { sut } = await makeSut();
+    const request = {
+      accountId: 'any_account_id',
+      name: undefined,
+      emoji: '🍹',
+    };
+    const response = await sut.handle(request);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new MissingParamError('name').message);
+  });
+});
