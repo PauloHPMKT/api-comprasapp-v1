6e9d74da71e1134a8b27cb820563be738066df28
Paulo Sergio <pauloserg861@gmail.com>
2025-09-06 19:08:41 -0300

    test: ensure AuthTokenMiddleware throw an error if no token is provided

--- src/main/middlewares/auth-token-middleware.spec.ts
+++ src/main/middlewares/auth-token-middleware.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthTokenMiddleware, TokenDecrypter } from './auth-token-middleware';
+import { Request } from 'express';
+import { UnauthorizedException } from '@nestjs/common';
 
 const makeMocks = () => ({
   tokenDecrypterMock: {
-    decrypt: jest.fn(),
+    decrypt: jest.fn().mockReturnValue('decoded_token'),
   },
 });
 
@@ -32,5 +34,15 @@ describe('AuthTokenMiddleware', () => {
     const { sut } = await makeSut();
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(AuthTokenMiddleware);
+  });
+
+  it('should throw UnauthorizedException if no authorization is provided', async () => {
+    const { sut } = await makeSut();
+    const req: Request = { headers: {} } as any;
+    const res = {} as any;
+    const next = jest.fn();
+
+    expect(() => sut.use(req, res, next)).toThrow('Token not provided');
+    expect(() => sut.use(req, res, next)).toThrow(UnauthorizedException);
   });
 });
