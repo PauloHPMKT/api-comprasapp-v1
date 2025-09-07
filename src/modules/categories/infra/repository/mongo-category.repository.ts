720b1345d483d2bd5d27095303024849fc1c3390
Paulo Sergio <pauloserg861@gmail.com>
2025-09-07 12:22:42 -0300

    feat: ensure MongoCategoryRepository return true if a category already exists

--- src/modules/categories/infra/repository/mongo-category.repository.ts
+++ src/modules/categories/infra/repository/mongo-category.repository.ts
@@ -1,10 +1,13 @@
 import { ObjectId } from 'mongodb';
 import { ToRepositoryModel } from '../../data/models/to-repository';
 import { CreateDefaultCategoriesRepositoryPort } from '../../data/ports/create-default-categories.repository';
+import { VerifyCategoryExistsRepository } from '../../data/ports/verify-category-exists';
 import { MongoHelper } from '@/modules/database/mongodb/helpers/mongo-helper';
 
 export class MongoCategoriesRepository
-  implements CreateDefaultCategoriesRepositoryPort
+  implements
+    CreateDefaultCategoriesRepositoryPort,
+    VerifyCategoryExistsRepository
 {
   async addCategories(data: ToRepositoryModel.Category[]): Promise<void> {
     const categories = data.map((category) => ({
@@ -16,5 +19,14 @@ export class MongoCategoriesRepository
     }));
     const categoryCollection = MongoHelper.getCollection('categories');
     await categoryCollection.insertMany(categories);
+  }
+
+  async verify(categoryName: string): Promise<boolean> {
+    const categoryCollection = MongoHelper.getCollection('categories');
+    const category = await categoryCollection.findOne(
+      { name: categoryName },
+      { projection: { _id: 1 } },
+    );
+    return !!category;
   }
 }
