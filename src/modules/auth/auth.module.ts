5b832bf9119cd5698084708188964bc3bcd56315
Paulo Sergio <pauloserg861@gmail.com>
2025-09-23 22:24:18 -0300

    test: ensure AuthController is defined

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './presentation/controllers/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [AuthController],
   providers: [],
   exports: [],
 })
