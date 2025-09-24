f985033ab5abc0a0ec2c1cca43b321c6c37a1382
Paulo Sergio <pauloserg861@gmail.com>
2025-09-23 22:39:51 -0300

    feat: ensure AuthController return badRequest if no email is provided

--- src/modules/auth/presentation/controllers/auth.controller.ts
+++ src/modules/auth/presentation/controllers/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { MissingParamError } from '@/shared/errors';
+import { badRequest } from '@/shared/presentation/helpers/http-response';
+import { HttpRequest, HttpResponse } from '@/shared/presentation/http';
+import { BaseController } from '@/shared/presentation/protocols/Controller';
+import { Controller, Post, Req } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController extends BaseController<any, any> {
+  @Post()
+  async handle(@Req() request: HttpRequest<any>): Promise<HttpResponse<any>> {
+    const requiredFields = ['email'];
+
+    const hasError = this.validateRequiredFields(request.body, requiredFields);
+    if (hasError) return badRequest(new MissingParamError(hasError));
+  }
+}
