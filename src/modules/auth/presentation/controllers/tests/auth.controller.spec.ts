652e5cca79e47565f0f5e7d7512b38e861a4ae03
Paulo Sergio <pauloserg861@gmail.com>
2025-09-23 22:40:04 -0300

    test: ensure AuthController return badRequest if no email is provided

--- src/modules/auth/presentation/controllers/tests/auth.controller.spec.ts
+++ src/modules/auth/presentation/controllers/tests/auth.controller.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from '@/shared/errors';
 import { AuthController } from '../auth.controller';
 import { makeAuthControllerSut } from './__mocks__/auth.controller-test-suite';
 
@@ -6,5 +7,18 @@ describe('AuthController', () => {
     const { sut } = await makeAuthControllerSut();
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(AuthController);
+  });
+
+  it('should return badRequest if no email is provided', async () => {
+    const { sut } = await makeAuthControllerSut();
+    const request = {
+      body: {
+        email: '',
+        password: 'anypassword',
+      },
+    };
+    const result = await sut.handle(request);
+    expect(result.statusCode).toBe(400);
+    expect(result.body).toBe(new MissingParamError('email').message);
   });
 });
