91639aa75516f6243d6e15705993a0c4e1c181d8
Paulo Sergio <pauloserg861@gmail.com>
2025-09-27 13:37:31 -0300

    feat: ensure MongoAccountRepository returns an customized account data on success

--- src/modules/account/infra/repository/mongo-account.repository.ts
+++ src/modules/account/infra/repository/mongo-account.repository.ts
@@ -1,12 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { MongoHelper } from '@/modules/database/mongodb/helpers/mongo-helper';
 import { CreateAccountRepositoryModel } from '../../data/models/create-account-repository.model';
 import { CreateAccountRepositoryPort } from '../../data/ports/create-account.repository';
 import { IsActiveAccountRepositoryPort } from '../../data/ports/is-active-account.repository';
 import { Injectable } from '@nestjs/common';
+import { GetAccountByUserIdRepositoryPort } from '@/modules/auth/data/repositories/get-account-by-user-is.repository';
+import { TokenPayloadModel } from '@/modules/auth/domain/models/token-payload';
 
 @Injectable()
 export class MongoAccountRepository
-  implements IsActiveAccountRepositoryPort, CreateAccountRepositoryPort
+  implements
+    IsActiveAccountRepositoryPort,
+    CreateAccountRepositoryPort,
+    GetAccountByUserIdRepositoryPort
 {
   async checkAccountByStatus(userId: string): Promise<boolean | null> {
     const accountCollection = MongoHelper.getCollection('accounts');
@@ -33,5 +39,36 @@ export class MongoAccountRepository
     return {
       id: insertedId.toHexString(),
     };
+  }
+
+  async getAccount(userId: string): Promise<TokenPayloadModel.Result | null> {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    const account = accountCollection.aggregate<TokenPayloadModel.Result>([
+      { $match: { userId: MongoHelper.toObjectId(userId) } },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'userId',
+          foreignField: '_id',
+          as: 'user',
+        },
+      },
+      { $unwind: '$user' },
+      {
+        $project: {
+          _id: 0,
+          id: '$_id',
+          name: '$user.name',
+          email: '$user.email',
+          avatar: '$user.avatar',
+          userId: '$user._id',
+          plan: '$plan',
+          password: '$password',
+          createdAt: '$createdAt',
+        },
+      },
+    ]);
+
+    return account.hasNext() ? await account.next() : null;
   }
 }
