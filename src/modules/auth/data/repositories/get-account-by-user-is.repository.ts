f87f8b7b805861f1aa91262e6bf61d9852a6f4d7
Paulo Sergio <pauloserg861@gmail.com>
2025-09-27 11:11:19 -0300

    feat: ensure ValidateUserService calls getAccountByUserIdRepositoryPort with correct id valid

---
+++ src/modules/auth/data/repositories/get-account-by-user-is.repository.ts
@@ -0,0 +1,5 @@
+import { TokenPayloadModel } from '../../domain/models/token-payload';
+
+export interface getAccountByUserIdRepositoryPort {
+  getAccount(userId: string): Promise<TokenPayloadModel.Params>;
+}
--- src/modules/auth/domain/services/validate-user.service.ts
+++ src/modules/auth/domain/services/validate-user.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ValidateUserCredentials } from '../../data/protocols/validate-user-credentials';
 import { TokenPayloadModel } from '../models/token-payload';
+import { getAccountByUserIdRepositoryPort } from '../../data/repositories/get-account-by-user-is.repository';
 
 @Injectable()
 export class ValidateUserService implements ValidateUserCredentials {
-  async validate({ email, password }): Promise<TokenPayloadModel.Params> {
-    console.log(email, password);
-    throw new Error('Method not implemented.');
+  constructor(
+    @Inject('GET_ACCOUNT_BY_USER_ID_REPOSITORY_PORT')
+    private readonly getAccountByUserIdRepository: getAccountByUserIdRepositoryPort,
+  ) {}
+
+  async validate({ id, password }): Promise<TokenPayloadModel.Params> {
+    await this.getAccountByUserIdRepository.getAccount(id);
+
+    return {
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      avatar: null,
+      accountId: 'valid_account_id',
+      plan: 'free',
+      createdAt: new Date('2025-09-27T01:56:39.666Z'),
+    };
   }
 }
