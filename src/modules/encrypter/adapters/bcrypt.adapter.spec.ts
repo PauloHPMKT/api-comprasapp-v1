e60da514133dd76533974975e4ca0cb90553db57
Paulo Sergio <pauloserg861@gmail.com>
2025-09-27 14:01:06 -0300

    test: ensure BcryptAdapter calls bcrypt with correct values

--- src/modules/encrypter/adapters/bcrypt.adapter.spec.ts
+++ src/modules/encrypter/adapters/bcrypt.adapter.spec.ts
@@ -5,6 +5,9 @@ jest.mock('bcrypt', () => ({
   async hash(): Promise<string> {
     return new Promise((resolve) => resolve('hashed_value'));
   },
+  async compare(): Promise<boolean> {
+    return new Promise((resolve) => resolve(true));
+  },
 }));
 
 const salt = 12;
@@ -41,5 +44,12 @@ describe('BcryptAdapter', () => {
       .mockImplementationOnce(() => Promise.reject(new Error()));
     const promise = sut.hash('any_value');
     await expect(promise).rejects.toThrow();
+  });
+
+  it('Should call bcript compare with correct values', async () => {
+    const { sut } = makeSut();
+    const compareSpy = jest.spyOn(bcrypt, 'compare');
+    await sut.compare('any_value', 'hashed_value');
+    expect(compareSpy).toHaveBeenCalledWith('any_value', 'hashed_value');
   });
 });
