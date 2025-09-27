1e87ab36b2a260d950995d8fd30e85e72ed3537e
Paulo Sergio <pauloserg861@gmail.com>
2025-09-27 14:00:59 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

--- src/modules/encrypter/adapters/bcrypt.adapter.ts
+++ src/modules/encrypter/adapters/bcrypt.adapter.ts
@@ -1,7 +1,8 @@
 import bcrypt from 'bcrypt';
 import { EncrypterPort } from '../domain/ports/encrypter.port';
+import { CompareIfPasswordIsValid } from '../domain/ports/compare-if-password-is-valid';
 
-export class BcryptAdapter implements EncrypterPort {
+export class BcryptAdapter implements EncrypterPort, CompareIfPasswordIsValid {
   private readonly salt: number;
 
   constructor(salt: number) {
@@ -11,5 +12,10 @@ export class BcryptAdapter implements EncrypterPort {
   async hash(value: string): Promise<string> {
     const hashedPassword = await bcrypt.hash(value, this.salt);
     return hashedPassword;
+  }
+
+  async compare(value: string, hashedValue: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(value, hashedValue);
+    return isValid;
   }
 }
